5a2e9ead4370604fa1ce95fc0dbe3b4ac49b22c4
nurul irfan <irfanlongstride@gmail.com>
2018-07-02 21:10:43 +0700

    iseng

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,5 +1,5 @@
 // const { assets } = global.serviceWorkerOption
-const CACHE = 'flsguide-cache'
+const CACHE = 'flsguide-cache-v1'
 
 // let assetsToCache = [...assets, './']
 let assetsToCache = serviceWorkerOption.assets
@@ -10,55 +10,77 @@ function precache() {
   });
 }
 
-function updateCache(request) {
-  return caches.open(CACHE).then(function (cache) {
-    return fetch(request).then(function (response) {
-      return cache.put(request, response)
-    })
-  })
-}
+// function updateCache(request) {
+//   return caches.open(CACHE).then(function (cache) {
+//     return fetch(request).then(function (response) {
+//       return cache.put(request, response)
+//     })
+//   })
+// }
 
-// Listeners
+// // Listeners
 
 self.addEventListener('install', function (evt) {
   console.log('The service worker is being installed.')
   evt.waitUntil(precache())
 })
 
+// self.addEventListener('fetch', function (evt) {
+//   evt.respondWith(fromNetwork(evt.request, 400).catch(function () {
+//       return fromCache(evt.request);
+//     })
+//   )
+// })
+
+
+// function fromNetwork(request, timeout) {
+//   console.log('serving from network: ', request.url)
+//   return new Promise(function (fulfill, reject) {
+//     // var timeoutId = setTimeout(reject, timeout);
+//     fetch(request).then(function (response) {
+//       // clearTimeout(timeoutId);
+//       fulfill(response.clone());
+//       caches.open(CACHE).then(function (cache) {
+//         if (request.url.indexOf('https://apifls.nurulirfan.com/images') >= 0) {
+//           cache.put(request.url, response)
+//         }
+//       })
+//     }, reject);
+//   });
+// }
+
+// function fromCache(request) {
+//   return caches.open(CACHE).then(function (cache) {
+//     return cache.match(request).then(function (matching) {
+//       console.log(matching)
+//       if (matching) {
+//         console.log('The service worker is serving the asset:', request.url);
+//         return matching
+//       } else {
+//         Promise.reject('no-match');
+//       }
+//     });
+//   });
+// }
+
 self.addEventListener('fetch', function (evt) {
-  evt.respondWith(fromNetwork(evt.request, 400).catch(function () {
-      return fromCache(evt.request);
-    })
-  )
+  console.log('The service worker is serving the asset.');
+  evt.respondWith(fromCache(evt.request));
+  evt.waitUntil(update(evt.request));
 })
-
-
-function fromNetwork(request, timeout) {
-  console.log('serving from network: ', request.url)
-  return new Promise(function (fulfill, reject) {
-    // var timeoutId = setTimeout(reject, timeout);
-    fetch(request).then(function (response) {
-      // clearTimeout(timeoutId);
-      fulfill(response.clone());
-      caches.open(CACHE).then(function (cache) {
-        if (request.url.indexOf('https://fonts.gstatic.com') >= 0) {
-          cache.put(request.url, response)
-        }
-      })
-    }, reject);
-  });
-}
 
 function fromCache(request) {
   return caches.open(CACHE).then(function (cache) {
     return cache.match(request).then(function (matching) {
-      console.log(matching)
-      if (matching) {
-        console.log('The service worker is serving the asset:', request.url);
-        return matching
-      } else {
-        Promise.reject('no-match');
-      }
+      return matching || Promise.reject('no-match');
+    });
+  });
+}
+
+function update(request) {
+  return caches.open(CACHE).then(function (cache) {
+    return fetch(request).then(function (response) {
+      return cache.put(request, response);
     });
   });
 }
